5ba64d820593b765a5bf5a84732f87c592ce66fa
Joseph Nguyen <joseph.t.nguyen99@gmail.com>
2023-07-01 16:07:35 +1000

    Refactored game settings to separate class

--- alien_invasion/game.py
+++ alien_invasion/game.py
@@ -1,28 +1,38 @@
 import pygame
 import sys
 
+from settings import Settings
+
 class Game:
     """
     Driver class
     """
     SCREEN_WIDTH, SCREEN_HEIGHT = 1200, 800
-    def __init__(self) -> None:
+    def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((Game.SCREEN_WIDTH, Game.SCREEN_HEIGHT))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_wdith, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run(self):
         """
         Main game loop
         """
-        while True:
+        running = True
+        while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    # sys.exit()  # TODO Which to use?
+                    running = False
         
+            # Redraw screen 
+            self.screen.fill(self.settings.bg_colour)
+
             # Display most recently drawn screen
             pygame.display.flip()
+        
+        # Once we break out of the loop, stop the game
+        pygame.quit()
+        # sys.exit()  # TODO Which to use?
 
 if __name__ == "__main__":
     game = Game()
---
+++ alien_invasion/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """
+    Class to store all settings for the game
+    """
+    def __init__(self):
+        self.screen_wdith = 1200
+        self.screen_height = 800
+        self.bg_colour = (230, 230, 230) # Light gray background colour
